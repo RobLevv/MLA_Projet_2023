6c8da505c9762bcac26e4feca2c51e11c5959557
linoxmo <ttmagoudi@gmail.com>
2023-11-30 11:13:49 +0100

    Little mistake correctio on the loss by Theo Magoudi

--- script/Loss_encoder_Théo.py
+++ script/Loss_encoder_Théo.py
@@ -1,18 +1,20 @@
 import numpy as np
 import pytorch
 
+
+
 # This first code is an introduction with the parameters without linked to the other code
  
 
 
- def eval_reconstruction_loss(data, param, auto_encoder): # data: Our data set   #param : Wich include the batch_size,
+def eval_reconstruction_loss(data, param, auto_encoder): # data: Our data set   #param : Wich include the batch_size,
         """
         Compute the loss 
         """
         auto_encoder.eval()  # set the auto_encoder to evaluation mode.
         batch_size= param.batch_size  
 
-        eval_reconstruction_loss = []     
+        loss = []     
         for i in range(0, len(data), batch_size): # for each iteration, we processes a new batch of data.
             batch_x, batch_y = data.evaluate(i, i + batch_size) # i = the begining of the batch i+batch_size= end of the batch
             outputs = auto_encoder(batch_x, batch_y) # We save for every iteration the outputs of our encoder
