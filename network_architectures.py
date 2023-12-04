4e8a69a1c429d8170bb01f0c1b523a02ea939532
eliot-christon <eliot.christon.1@etu.sorbonne-universite.fr>
2023-12-04 23:27:50 +0100

    fix pytorch architectures

--- network_architectures.py
+++ network_architectures.py
@@ -17,93 +17,112 @@ N_ATTRIBUTES = 40
 #%% Encoder
 # The encoder consists of the following 7 layers:
 # C16 − C32 − C64 − C128 − C256 − C512 − C512
-encoder = Sequential([
-    Conv2d(16, (4, 4), strides=(2, 2), padding='same', input_shape=IMG_SIZE),
-    BatchNorm2d(),
-    LeakyReLU(0.2),
-    
-    Conv2d(32, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    LeakyReLU(0.2),
-    
-    Conv2d(64, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    LeakyReLU(0.2),
-    
-    Conv2d(128, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    LeakyReLU(0.2),
-    
-    Conv2d(256, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    LeakyReLU(0.2),
-    
-    Conv2d(512, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    LeakyReLU(0.2),
-    
-    Conv2d(512, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    LeakyReLU(0.2),
-])
+encoder_layers = Sequential(
+    # C16: Convolutional layer with 16 filters
+    Conv2d(in_channels=IMG_SIZE[2], out_channels=16, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(16),
+    LeakyReLU(0.2, inplace=True),
+
+    # C32: Convolutional layer with 32 filters
+    Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(32),
+    LeakyReLU(0.2, inplace=True),
+
+    # C64: Convolutional layer with 64 filters
+    Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(64),
+    LeakyReLU(0.2, inplace=True),
+
+    # C128: Convolutional layer with 128 filters
+    Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(128),
+    LeakyReLU(0.2, inplace=True),
+
+    # C256: Convolutional layer with 256 filters
+    Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(256),
+    LeakyReLU(0.2, inplace=True),
+
+    # C512: Convolutional layer with 512 filters
+    Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(512),
+    LeakyReLU(0.2, inplace=True),
+
+    # C512: Convolutional layer with 512 filters
+    Conv2d(in_channels=512, out_channels=512, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(512),
+    LeakyReLU(0.2, inplace=True)
+)
+
 
 print("Encoder Summary:")
-encoder.summary()
+print(encoder_layers)
 
 
 #%% Decoder
 # the decoder is symmetric to the encoder, but uses transposed convolutions for the up-sampling:
 # C512+2n − C512+2n − C256+2n − C128+2n − C64+2n − C32+2n − C16+2n .
-decoder = Sequential([
-    ConvTranspose2d(512+2*N_ATTRIBUTES, (4, 4), strides=(2, 2), padding='same', input_shape=(2, 2, 512)),
-    BatchNorm2d(),
-    ReLU(),
-    
-    ConvTranspose2d(512+2*N_ATTRIBUTES, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    ReLU(),
-    
-    ConvTranspose2d(256+2*N_ATTRIBUTES, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    ReLU(),
-    
-    ConvTranspose2d(128+2*N_ATTRIBUTES, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    ReLU(),
-    
-    ConvTranspose2d(64+2*N_ATTRIBUTES, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    ReLU(),
-    
-    ConvTranspose2d(32+2*N_ATTRIBUTES, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    ReLU(),
-    
-    ConvTranspose2d(16+2*N_ATTRIBUTES, (4, 4), strides=(2, 2), padding='same'),
-    BatchNorm2d(),
-    ReLU(),
-])
+decoder_layers = Sequential(
+    # C512+2n: Transposed convolutional layer with 512 + 2 * N_ATTRIBUTES filters
+    ConvTranspose2d(in_channels=512 + 2 * N_ATTRIBUTES, out_channels=512, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(512),
+    ReLU(inplace=True),
+
+    # C512+2n: Transposed convolutional layer with 512 + 2 * N_ATTRIBUTES filters
+    ConvTranspose2d(in_channels=512 + 2 * N_ATTRIBUTES, out_channels=512, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(512),
+    ReLU(inplace=True),
+
+    # C256+2n: Transposed convolutional layer with 256 + 2 * N_ATTRIBUTES filters
+    ConvTranspose2d(in_channels=512 + 2 * N_ATTRIBUTES, out_channels=256, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(256),
+    ReLU(inplace=True),
+
+    # C128+2n: Transposed convolutional layer with 128 + 2 * N_ATTRIBUTES filters
+    ConvTranspose2d(in_channels=256 + 2 * N_ATTRIBUTES, out_channels=128, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(128),
+    ReLU(inplace=True),
+
+    # C64+2n: Transposed convolutional layer with 64 + 2 * N_ATTRIBUTES filters
+    ConvTranspose2d(in_channels=128 + 2 * N_ATTRIBUTES, out_channels=64, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(64),
+    ReLU(inplace=True),
+
+    # C32+2n: Transposed convolutional layer with 32 + 2 * N_ATTRIBUTES filters
+    ConvTranspose2d(in_channels=64 + 2 * N_ATTRIBUTES, out_channels=32, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(32),
+    ReLU(inplace=True),
+
+    # C16+2n: Transposed convolutional layer with 16 + 2 * N_ATTRIBUTES filters
+    ConvTranspose2d(in_channels=32 + 2 * N_ATTRIBUTES, out_channels=16, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(16),
+    ReLU(inplace=True)
+)
 
 
 print("Decoder Summary:")
-decoder.summary()
-
+print(decoder_layers)
 
 #%% Discriminator
 # The discriminator is a C512 layer followed by a fully-connected neural network of two layers of size 512 and n repsectively.
-discriminator = Sequential([
-    Conv2d(512, (4, 4), strides=(2, 2), padding='same', input_shape=IMG_SIZE),
-    BatchNorm2d(),
-    LeakyReLU(0.2),
+discriminator_layers = Sequential(
+    # C512: Convolutional layer with 512 filters
+    Conv2d(in_channels=IMG_SIZE[2], out_channels=512, kernel_size=4, stride=2, padding=1),
+    BatchNorm2d(512),
+    LeakyReLU(0.2, inplace=True),
     
+    # Flatten the output for fully-connected layers
     Flatten(),
-    Linear(512),
-    BatchNorm2d(),
-    LeakyReLU(0.2),
-    
-    Linear(N_ATTRIBUTES),  # n is the number of attributes
-])
+
+    # Fully-connected layer with size 512
+    Linear(512, 512),
+    LeakyReLU(0.2, inplace=True),
+
+    # Fully-connected layer with size n (assuming n is the number of attributes)
+    Linear(512, N_ATTRIBUTES)
+)
+
 
 print("Discriminator Summary:")
-discriminator.summary()
+print(discriminator_layers)
 # %%
