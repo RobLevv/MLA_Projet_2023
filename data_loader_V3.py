6ebb5d4110a631217d3a270633be86e68ab31d8c
eliot-christon <eliot.christon.1@etu.sorbonne-universite.fr>
2023-12-06 14:37:04 +0100

    train_val_test_split completed

--- data_loader_V3.py
+++ data_loader_V3.py
@@ -18,7 +18,7 @@ def transform_img_for_celeba(sample:dict, target_size:int=256) -> dict:
     # Crop the image to make it square
     img = crop(img, 40, 0, IMG_SIZE[0], IMG_SIZE[0])
     img = resize(img, (target_size, target_size), antialias=True)
-    return {'image': img, 'attributes': sample['attributes']}
+    return {'image': img, 'attributes': sample['attributes'], 'image_name': sample['image_name']}
 
 
 class ImgDataset(Dataset):
@@ -43,7 +43,7 @@ class ImgDataset(Dataset):
 
     def __getitem__(self, idx):
         """
-        Returns a sample of the dataset. {'image': image, 'attributes': attributes}
+        Returns a sample of the dataset. {'image': image, 'attributes': attributes, 'image_name': image_name}
         """
         if torch.is_tensor(idx):
             idx = idx.tolist()
@@ -53,7 +53,7 @@ class ImgDataset(Dataset):
         image = read_image(img_name)
         attributes = self.attributes_df.iloc[idx, 1:]
         attributes = np.array([attributes], dtype=float).reshape(-1)
-        sample = {'image': image, 'attributes': attributes}
+        sample = {'image': image, 'attributes': attributes, 'image_name': img_name}
 
         if self.transform:
             sample = self.transform(sample)
@@ -62,75 +62,34 @@ class ImgDataset(Dataset):
 
 
 
-if __name__ == "__main__":
-    dataset = ImgDataset(attributes_csv_file='data/Anno/list_attr_celeba.txt', img_root_dir='data/Img')
-
-    print(len(dataset))
-    
-    sample0 = dataset[0]
-    im0, attr0 = sample0['image'], sample0['attributes']
-    print("Image 0 shape:", im0.shape)
-    print("Attributes 0 shape:", attr0.shape)
-    
-    
-    import matplotlib.pyplot as plt
-    
-    N = 8
-    
-    
-    fig, ax = plt.subplots(2, N, figsize=(1.7*N, 4))
-    
-    for idx in range(N):
-        image, attributes = dataset[idx]['image'], dataset[idx]['attributes']
-        ax[0, idx].imshow(image.permute(1, 2, 0))
-        ax[0, idx].axis('off')
-        ax[0, idx].set_title("Image {}".format(dataset.attributes_df.iloc[idx, 0]), fontsize=8)
-        
-        text = ""
-        for i, attr in enumerate(attributes):
-            if attr == 1:
-                text += dataset.attributes_df.columns[i+1] + '\n'
-                
-        ax[1, idx].text(0.5, 0.5, text, horizontalalignment='center', verticalalignment='center', fontsize=8)
-        ax[1, idx].axis('off')
-    
-    
-    plt.tight_layout()
-    plt.show()
-    
-    
-    # OR with a dataloader
-    print("\nWith a dataloader:\n")
-    dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
-    
-    for i, sample in enumerate(dataloader):
-        images, attributes = sample['image'], sample['attributes']
-        print("Batch {}:".format(i))
-        print("Images shape:", images.shape)
-        print("Attributes shape:", attributes.shape)
-        
-        
 def train_validation_test_split(
         dataset:Dataset,
-        train_split:float=0.803,
-        test_split:float=0.099,
-        val_split:float=0.098,
-        shuffle:bool=False
-        ) -> tuple([dataset,dataset,dataset]):
+        train_split:float=0.80349,
+        test_split:float=0.0985296,
+        val_split:float=0.0980607,
+        shuffle:bool=False,
+        display:bool=False
+        ) -> tuple([Dataset,Dataset,Dataset]):
     
-    assert int(100*(train_split + test_split + val_split)) == 1, "train_split + test_split + val_split must be equals to 1"
+    assert int(100*(train_split + test_split + val_split)) == 100, "train_split + test_split + val_split must be equals to 1, not {}".format(train_split + test_split + val_split)
 
     idx = list(range(len(dataset)))
 
-    nb_val = np.round(len(dataset)*val_split)
-    nb_test = np.round(len(dataset)*test_split)
+    nb_val = int(np.round(len(dataset)*val_split))
+    nb_test = int(np.round(len(dataset)*test_split))
     nb_train = len(dataset) - (nb_val + nb_test)
+    
+    if display:
+        print("nb_train:", nb_train)
+        print("nb_val  :", nb_val)
+        print("nb_test :", nb_test)
+        
+        print("sum     :", nb_train + nb_val + nb_test)
 
     if shuffle:
         np.random.shuffle(idx)
-        print(idx[:10])
         idx_train = idx_train = idx[:nb_train]
-        idx_val = idx[nb_train:nb_val]
+        idx_val = idx[nb_train:nb_train+nb_val]
         idx_test = idx[nb_train+nb_val:]
     else:
         idx_train = idx[:nb_train]
@@ -140,4 +99,105 @@ def train_validation_test_split(
     train_dataset = Subset(dataset, idx_train)
     val_dataset= Subset(dataset,idx_val)
     test_dataset= Subset(dataset,idx_test)
+    
     return train_dataset,val_dataset,test_dataset
+
+
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    
+    def main1():
+        """Test the ImgDataset class."""
+        
+        print("DATASET TEST\n")
+        
+        dataset = ImgDataset(attributes_csv_file='data/Anno/list_attr_celeba.txt', img_root_dir='data/Img')
+
+        print(len(dataset))
+        
+        sample0 = dataset[0]
+        im0, attr0 = sample0['image'], sample0['attributes']
+        print("Image 0 shape:", im0.shape)
+        print("Attributes 0 shape:", attr0.shape)
+        
+        N = 8
+        
+        
+        fig, ax = plt.subplots(2, N, figsize=(1.7*N, 4))
+        
+        for idx in range(N):
+            image, attributes = dataset[idx]['image'], dataset[idx]['attributes']
+            ax[0, idx].imshow(image.permute(1, 2, 0))
+            ax[0, idx].axis('off')
+            ax[0, idx].set_title("Image {}".format(dataset.attributes_df.iloc[idx, 0]), fontsize=8)
+            
+            text = ""
+            for i, attr in enumerate(attributes):
+                if attr == 1:
+                    text += dataset.attributes_df.columns[i+1] + '\n'
+                    
+            ax[1, idx].text(0.5, 0.5, text, horizontalalignment='center', verticalalignment='center', fontsize=8)
+            ax[1, idx].axis('off')
+        
+        
+        plt.tight_layout()
+        plt.show()
+    
+    
+    def main2():
+        """Test the ImgDataset class with a dataloader."""
+        # OR with a dataloader
+        print("\nWith a dataloader:\n")
+        dataset = ImgDataset(attributes_csv_file='data/Anno/list_attr_celeba.txt', img_root_dir='data/Img')
+        dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
+        
+        for i, sample in enumerate(dataloader):
+            images, attributes = sample['image'], sample['attributes']
+            print("Batch {}:".format(i))
+            print("Images shape:", images.shape)
+            print("Attributes shape:", attributes.shape)
+            break
+        
+        
+        print("\n\nSPLIT TEST\n")
+    
+    
+    def main3():
+        """Test the train_validation_test_split function."""
+        dataset = ImgDataset(attributes_csv_file='data/Anno/list_attr_celeba.txt', img_root_dir='data/Img')
+    
+        train_dataset, val_dataset, test_dataset = train_validation_test_split(dataset, shuffle=True, display=True)
+        
+        # let's explore the datasets
+        print("TRAIN DATASET")
+        print("len:", len(train_dataset))
+        print("sample 0 attributes:", train_dataset[0]['attributes'])
+        print('image name:', train_dataset[0]['image_name'])
+        
+        print("VAL DATASET")
+        print("len:", len(val_dataset))
+        print("sample 0 attributes:", val_dataset[0]['attributes'])
+        print('image name:', val_dataset[0]['image_name'])
+        
+        print("TEST DATASET")
+        print("len:", len(test_dataset))
+        print("sample 0 attributes:", test_dataset[0]['attributes'])
+        print('image name:', test_dataset[0]['image_name'])
+        
+        plt.figure(figsize=(5, 5))
+        plt.pie(
+            [len(train_dataset), len(val_dataset), len(test_dataset)],
+            labels=["Train", "Validation", "Test"], 
+            autopct='%1.1f%%',
+            startangle=90,
+            textprops={'fontsize': 14}
+        )
+        plt.title("Partition Distribution")
+        plt.show()
+    
+    
+    ## MAIN ##
+    
+    main3()
