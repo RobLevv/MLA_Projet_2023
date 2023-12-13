7df834621553374c94e058e561c5a2bfd7dcb36e
Adrien P <petard.adrien@gmail.com>
2023-12-13 17:06:21 +0100

    lambda_ae + Loss_ae

--- objectives.py
+++ objectives.py
@@ -16,12 +16,12 @@ def discriminator_objective(y, y_pred):
     return torch.nn.functional.binary_cross_entropy_with_logits(y_pred, y).float()
 
 
-def adversarial_objective(x, x_recon, y, y_discriminated, lamb):
+def adversarial_objective(x, x_recon, y, y_discriminated, lambda_ae=0.9):
     """
     x: image
     x_recon: reconstructed image
     y: attributes
     y_discriminated: predicted attributes
-    lamb: lambda parameter
+    lambda_ae: lambda auto-encoder parameter
     """
-    return reconstruction_objective(x, x_recon) - lamb * discriminator_objective(1 - y, y_discriminated)
+    return reconstruction_objective(x, x_recon) + lambda_ae * discriminator_objective(1 - y, y_discriminated)
