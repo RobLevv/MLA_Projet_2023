74c5c9b7af588704d2b46ce7f4457e1a91d2e4e4
eliot-christon <eliot.christon.1@etu.sorbonne-universite.fr>
2023-12-14 10:51:54 +0100

    display change minor

--- training_loop.py
+++ training_loop.py
@@ -50,7 +50,7 @@ def train_loop(
         
         current_time = time.time() - start_time
         
-        print("Epoch : " + str(epoch + 1) + "/" + str(n_epochs) + " time: " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time)))
+        print("\nEpoch : " + str(epoch + 1) + "/" + str(n_epochs) + " time: " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time)))
         
         pbar = tqdm.tqdm(enumerate(data_loader), total=len(data_loader), ncols=100)
         
@@ -110,6 +110,7 @@ def train_loop(
                 )
         
         epoch_loss /= len(data_loader)
+        pbar.close()
     
     current_time = time.time() - start_time
     
