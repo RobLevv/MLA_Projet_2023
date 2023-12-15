6aa18c272546d83187eff55a53a2b385dada5a9e
eliot-christon <eliot.christon.1@etu.sorbonne-universite.fr>
2023-12-15 20:04:29 +0100

    fix objectives

--- objectives.py
+++ objectives.py
@@ -5,7 +5,7 @@ def reconstruction_objective(x, x_recon):
     x: image
     x_recon: reconstructed image
     """
-    return torch.nn.functional.mse_loss(x.float(), x_recon).float()
+    return torch.nn.functional.mse_loss(x.float(), x_recon.float()).float()
 
 
 def discriminator_objective(y, y_pred):
@@ -13,7 +13,7 @@ def discriminator_objective(y, y_pred):
     y: attributes
     y_discriminated: predicted attributes
     """
-    return torch.nn.functional.binary_cross_entropy_with_logits(y_pred, y).float()
+    return torch.nn.functional.binary_cross_entropy(y_pred.float(), y.float()).float()
 
 
 def adversarial_objective(x, x_recon, y, y_discriminated, lambda_ae=0.9):
@@ -24,4 +24,4 @@ def adversarial_objective(x, x_recon, y, y_discriminated, lambda_ae=0.9):
     y_discriminated: predicted attributes
     lambda_ae: lambda auto-encoder parameter
     """
-    return reconstruction_objective(x, x_recon) - lambda_ae * discriminator_objective(1 - y, y_discriminated)
+    return reconstruction_objective(x, x_recon) - lambda_ae * discriminator_objective(1 - y, 1 - y_discriminated)
